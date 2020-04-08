2e21b9c9d6c26a421a1ce72fd028ac80d887bf43
rosie w <rosie.wilt@gmail.com>
2020-04-08 16:57:18 +0000

    Automatically backed up by Learn

--- lib/updating_nested_hashes.rb
+++ lib/updating_nested_hashes.rb
@@ -25,7 +25,6 @@ def update_status
 def update_status
   epic_tragedy[:montague][:hero][:status] = "dead"
   epic_tragedy[:montague][:heroine][:status] = "dead"
-  epic_tragedy
 end
 
 def add_characters(hash)
